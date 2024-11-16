ccfa0c1de10d23cc0b466105ec30b5c40d89b080
uuizdashdw <chany920@gmail.com>
2024-11-16 15:37:56 +0900

    1. gesture-handler 업데이트
    2. AppDelegate.h 에 gesture-handler 관련 내용 추가

--- MatzipApp/android/app/src/main/java/com/matzipapp/MainActivity.java
+++ MatzipApp/android/app/src/main/java/com/matzipapp/MainActivity.java
@@ -1,38 +1,67 @@
-package com.matzipapp;
+// package com.matzipapp;
 
-import com.facebook.react.ReactActivity;
-import android.os.Bundle;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
+// import com.facebook.react.ReactActivity;
+// import android.os.Bundle;
+// import com.facebook.react.ReactActivityDelegate;
+// import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+// import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+// public class MainActivity extends ReactActivity {
+
+//   /**
+//    * Returns the name of the main component registered from JavaScript. This is used to schedule
+//    * rendering of the component.
+//    */
+//   @Override
+//   protected String getMainComponentName() {
+//     return "MatzipApp";
+//   }
+
+//   /**
+//    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+//    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+//    * (aka React 18) with two boolean flags.
+//    */
+//   @Override
+//   protected ReactActivityDelegate createReactActivityDelegate() {
+//     return new DefaultReactActivityDelegate(
+//         this,
+//         getMainComponentName(),
+//         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+//         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+//   }
+
+//   @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(null);
+//   }
+// }
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // 추가
 
 public class MainActivity extends ReactActivity {
+    
+    @Override
+    protected String getMainComponentName() {
+        return "MatzipApp";
+    }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "MatzipApp";
-  }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            DefaultNewArchitectureEntryPoint.getFabricEnabled()
+        );
+    }
 
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
-   */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
-  }
+    @Override
+    protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(this); // 추가
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
 }
--- MatzipApp/ios/MatzipApp/AppDelegate.h
+++ MatzipApp/ios/MatzipApp/AppDelegate.h
@@ -1,6 +1,27 @@
+// #import <RCTAppDelegate.h>
+// #import <UIKit/UIKit.h>
+
+// @interface AppDelegate : RCTAppDelegate
+
+// @end
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <ReactNativeGestureHandler/ReactNativeGestureHandler.h> // 추가
 
 @interface AppDelegate : RCTAppDelegate
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [RNGestureHandlerRootView class]; // 추가: 제스처 핸들러 활성화
+    // 나머지 설정 그대로 유지
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
 
 @end

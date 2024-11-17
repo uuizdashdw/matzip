859de60615f36dc1dd05236a2d4ebe5045ec0b0b
uuizdashdw <chany920@gmail.com>
2024-11-17 19:00:29 +0900

    1. 로그인, 회원가입 버튼 위 이미지 적용
    2. 이미지 적용에 따른 스타일링

--- MatzipApp/src/components/CustomButton.tsx
+++ MatzipApp/src/components/CustomButton.tsx
@@ -30,14 +30,14 @@ function CustomButton({
       disabled={inValid}
       style={({ pressed }) => [
         styles.container,
-
-        styles[size],
         pressed ? styles[`${variant}Pressed`] : styles[variant],
         ,
         inValid && styles.inValid,
       ]}
       {...props}>
-      <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      <View style={styles[size]}>
+        <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      </View>
     </Pressable>
   );
 }
@@ -46,6 +46,7 @@ const styles = StyleSheet.create({
   container: {
     borderRadius: 3,
     justifyContent: 'center',
+    flexDirection: 'row',
   },
 
   inValid: {
@@ -65,12 +66,14 @@ const styles = StyleSheet.create({
     paddingVertical: deviceHeight > 700 ? 15 : 10,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
   medium: {
     width: '50%',
     paddingVertical: deviceHeight > 700 ? 12 : 8,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
 
   text: {
